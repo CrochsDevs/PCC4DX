a2266080bda2253e5e3d7f7a4eb7c40a2b77b316
immortal1sm <solomonallan27@gmail.com>
2025-04-22 09:39:00 +0800

    sql dump

---
+++ database/dump_partners.sql
@@ -0,0 +1,21 @@
+INSERT INTO pcc_auth_system.partners (
+  partner_name, herd_code, contact_person, contact_number, municipality, province,
+  is_active, coop_type, center_code
+) VALUES
+('Abra Farmers and Provincial Employees Multi-purpose Cooperative (AFPEMCO)', 'HRD5823', 'Maria Santos', '09171234567', 'Bangued', 'Abra', 1, 'Cooperatives', 'MMSU'),
+('Apayao Livestock Agriculture Cooperative (ALAC)', 'HD8942', 'Juan Dela Cruz', '09981237654', 'Luna', 'Apayao', 1, 'Associations', 'CSU'),
+('LGU- Itogon, Benguet', 'HDC2211', 'Elena Ramos', '09181230987', 'Itogon', 'Benguet', 0, 'LGU', 'DMMMSU'),
+('Unknown Partner - Ifugao', 'HRD3345', 'Carlos Reyes', '09075678912', 'Ifugao City', 'Ifugao', 0, 'SUC', 'DMMMSU'),
+('Unknown Partner - Kalinga', 'HDC9090', 'Liza Fernandez', '09192345678', 'Kalinga City', 'Kalinga', 0, 'LGU', 'DMMMSU'),
+('Mountain Province State Polytechnic College', 'HRD7781', 'Ricardo Aquino', '09176543210', 'Bontoc', 'Mt. Province', 0, 'SUC', 'DMMMSU'),
+('PCC at MMSU IH', 'HDC2345', 'Ana Garcia', '09271239876', 'Batac', 'Ilocos Norte', 0, 'SUC', 'MMSU'),
+('PCC at MMSU CBED DSWD Sterilized', 'HD3388', 'Miguel Torres', '09391234560', 'Batac', 'Ilocos Norte', 0, 'SUC', 'MMSU'),
+('PCC at MMSU CBED Deped Sterilized', 'HRD5412', 'Josephine Cruz', '09199887766', 'Batac', 'Ilocos Norte', 0, 'SUC', 'MMSU'),
+('Camguidan SLP Asso.', 'HDC1203', 'Alfredo Manalo', '09061234567', 'Batac', 'Ilocos Norte', 0, 'Associations', 'MMSU'),
+('Ilocano Artificial Inseminators Credit Cooperative', 'HD7851', 'Ramon Mendoza', '09173451239', 'Solsona', 'Ilocos Norte', 1, 'Cooperatives', 'MMSU'),
+('Marcos Dairy Raisers Association', 'HRD6019', 'Glenda Morales', '09181239876', 'Marcos', 'Ilocos Norte', 1, 'Associations', 'MMSU'),
+('Ilocano Artificial Inseminators Credit Cooperative', 'HD2234', 'George Alvarado', '09191234009', 'San Nicolas', 'Ilocos Norte', 1, 'Cooperatives', 'MMSU'),
+('Timpuyog de San Marcelino SLPA', 'HDC8842', 'Isabel Navarro', '09221234567', 'Dingras', 'Ilocos Norte', 1, 'Family Module', 'MMSU'),
+('Pangnamnamaan ti Colo Multipurpose Cooperative', 'HD5490', 'Cristina Enriquez', '09171230145', 'Batac', 'Ilocos Norte', 1, 'Cooperatives', 'MMSU'),
+('Northern Ilocos Sur Agriculture Cooperative (NEW)', 'HRD9812', 'Marcelo Garcia', '09170001234', 'Cabugao', 'Ilocos Sur', 1, 'Corporation', 'MMSU');
+
