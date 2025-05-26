93f5b8cc289dcf035e4ba632172469294b44e1b1
immortal1sm <solomonallan27@gmail.com>
2025-05-26 14:04:24 +0800

    CD admin report

--- admin_report_calf_dashboard.php
+++ admin_report_calf_dashboard.php
@@ -152,7 +152,7 @@ $allCenters = $reportManager->getAllCenters();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HQ AI Score Card Dashboard</title>
+    <title>HQ Calf Drop Dashboard</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
